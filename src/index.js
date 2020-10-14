384a1bf40ee4abaee50134b60a95e1b91af5652e
Juan Camilo González <info@juancgonzalez.com>
2020-10-14 14:30:03 -0500

    🚀  v1

--- src/index.js
+++ src/index.js
@@ -1,2 +1,33 @@
 import './scss/styles.scss';
-console.log('tick');
+
+function crearMenu() {
+  const menu = document.getElementById('menu');
+  const paginas = document.querySelectorAll('.pagina');
+  let paginaActual = paginas[0];
+
+  for (let i = 0; i < paginas.length; i++) {
+    const numPagina = i + 1;
+    const boton = document.createElement('span');
+    const nombre = paginas[i].dataset.nombre;
+    boton.className = i === 0 ? 'boton-menu actual' : 'boton-menu';
+    boton.innerText = nombre ? nombre : `Página ${numPagina}`;
+
+    boton.addEventListener('click', function () {
+      menu.querySelector('.actual').classList.remove('actual');
+      boton.classList.add('actual');
+      paginaActual.classList.add('esconder');
+      paginas[i].classList.remove('esconder');
+      paginaActual = paginas[i];
+
+      if (numPagina === 4) {
+        cargarMapa();
+      } else if (numPagina === 5) {
+        cargarClima();
+      }
+    });
+
+    menu.appendChild(boton);
+  }
+}
+
+crearMenu();
